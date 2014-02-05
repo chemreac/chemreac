1a8ac24969e43085a61cfb2739d33dc742d1f1c4
Björn Dahlgren <bjodah@gmail.com>
2014-02-05 17:16:23 +0100

    Added tests of logy and logt for 'four species' system.

--- chemreac/tests/test_four_species.py
+++ chemreac/tests/test_four_species.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import os
+from itertools import product
 
 import numpy as np
+import pytest
 
 from chemreac.serialization import load
 
@@ -17,57 +19,103 @@ chemreac.PyReactionDiffusion.f
 chemreac.PyReactionDiffusion.dense_jac_rmaj
 chemreac.PyReactionDiffusion.dense_jac_cmaj
 
+See:
+<four_species_f_jac.png>
+<four_species_f_jac_logy.png>
+<four_species_f_jac_logt.png>
 """
 
 # A -> B               k1=0.05
 # 2C + B -> D + B      k2=3.0
 
-json_path = os.path.join(os.path.dirname(__file__), 'four_species.json')
+JSON_PATH = os.path.join(os.path.dirname(__file__), 'four_species.json')
+TRUE_FALSE_PAIRS = list(product([True, False], [True, False]))
 
-def _get_ref_J(sys, y0, order='C'):
+
+def _get_ref_f(sys, t0, y0, logy, logt):
     k1, k2 = sys.k
     A, B, C, D = y0
-    ref_J = np.array([[-k1,         0,           0, 0],
-                      [ k1,         0,           0, 0],
-                      [  0, -2*k2*C*C, -2*2*k2*C*B, 0],
-                      [  0,    k2*C*C,    2*k2*C*B, 0]],
-                     order=order)
+
+    ref_f = np.array([-k1*A, k1*A, -2*k2*C*C*B, k2*C*C*B])
+    if logy:
+        ref_f /= y0
+    if logt:
+        ref_f *= t0
+    return ref_f
+
+
+def _get_ref_J(sys, t0, y0, logy, logt, order='C'):
+    k1, k2 = sys.k
+    A, B, C, D = y0
+    if logy:
+        f1,f2,f3,f4 = _get_ref_f(sys, t0, y0, logy, logt)
+        ref_J = np.array(
+            [[   0,   0,    0,   0],
+             [  f2, -f2,    0,   0],
+             [   0,  f3,   f3,   0],
+             [   0,  f4, 2*f4, -f4]],
+            order=order)
+    else:
+        ref_J = np.array(
+            [[-k1,         0,           0, 0],
+             [ k1,         0,           0, 0],
+             [  0, -2*k2*C*C, -2*2*k2*C*B, 0],
+             [  0,    k2*C*C,    2*k2*C*B, 0]],
+            order=order)
+        if logt:
+            ref_J *= t0
+
     return ref_J
 
 
-def test_f():
+@pytest.mark.parametrize("log", TRUE_FALSE_PAIRS)
+def test_f(log):
+    logy, logt = log
     N=1
-    sys = load(json_path, N=N)
+    sys = load(JSON_PATH, N=N, logy=logy, logt=logt)
     y0 = np.array([1.3, 1e-4, 0.7, 1e-4])
-    t0 = 0.0
+    t0 = 42.0
 
-    k1, k2 = sys.k
-    A, B, C, D = y0
-    ref_f = np.array([-k1*A, k1*A, -2*k2*C*C*B, k2*C*C*B])
+    ref_f = _get_ref_f(sys, t0, y0, logy, logt)
     fout = np.empty_like(ref_f)
-    sys.f(t0, y0, fout)
+
+    y = np.log(y0) if logy else y0
+    t = np.log(t0) if logt else t0
+    sys.f(t, y, fout)
     assert np.allclose(fout, ref_f)
 
 
-def test_dense_jac_rmaj():
+@pytest.mark.parametrize("log", TRUE_FALSE_PAIRS)
+def test_dense_jac_rmaj(log):
+    logy, logt = log
     N=1
-    sys = load(json_path, N=N)
+    sys = load(JSON_PATH, N=N, logy=logy, logt=logt)
     y0 = np.array([1.3, 1e-4, 0.7, 1e-4])
-    t0 = 0.0
+    t0 = 42.0
 
-    ref_J = _get_ref_J(sys, y0)
+    ref_J = _get_ref_J(sys, t0, y0, logy, logt)
     Jout = np.empty_like(ref_J)
-    sys.dense_jac_rmaj(t0, y0, Jout)
+
+    y = np.log(y0) if logy else y0
+    t = np.log(t0) if logt else t0
+    sys.dense_jac_rmaj(t, y, Jout)
+
     assert np.allclose(Jout, ref_J)
 
 
-def test_dense_jac_cmaj():
+@pytest.mark.parametrize("log", TRUE_FALSE_PAIRS)
+def test_dense_jac_cmaj(log):
+    logy, logt = log
     N=1
-    sys = load(json_path, N=N)
+    sys = load(JSON_PATH, N=N, logy=logy, logt=logt)
     y0 = np.array([1.3, 1e-4, 0.7, 1e-4])
-    t0 = 0.0
+    t0 = 42.0
 
-    ref_J = _get_ref_J(sys, y0, order='F')
+    ref_J = _get_ref_J(sys, t0, y0, logy, logt, order='F')
     Jout = np.empty_like(ref_J)
-    sys.dense_jac_cmaj(t0, y0, Jout)
+
+    y = np.log(y0) if logy else y0
+    t = np.log(t0) if logt else t0
+    sys.dense_jac_cmaj(t, y, Jout)
+
     assert np.allclose(Jout, ref_J)
