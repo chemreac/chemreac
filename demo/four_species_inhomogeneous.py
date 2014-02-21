48dea1ff4dd80854683440362db4c0642b98f19a
Björn Dahlgren <bjodah@gmail.com>
2014-02-21 17:45:44 +0100

    Updated demo/four_species_inhomogeneous.py to new run() sig + plot 3D

--- demo/four_species_inhomogeneous.py
+++ demo/four_species_inhomogeneous.py
@@ -5,7 +5,11 @@ from __future__ import print_function, division
 
 import argh
 import numpy as np
-import matplotlib.pyplot as plt
+# matplotlib
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib
+from matplotlib import cm
+from matplotlib import pyplot as plt
 
 from chemreac.serialization import load
 from chemreac import DENSE, BANDED, SPARSE
@@ -21,23 +25,46 @@ Demo of chemical reaction diffusion system.
 # 2C + B -> D + B      k2=3.0
 
 
-def main(tend=10.0, N=10, nt=50, plot=True, spy=False, mode=None):
+def main(tend=3.0, N=30, nt=30, plot=False, mode=None, 
+         logy=False, logt=False, show=False):
     mod1 = lambda x: x/(x**2+1)
-    sys = load('four_species.json', N=N, x=N,
-               bin_k_factor=[[mod1(x)] for x in range(N)], # decay A->B is modulated with x
+    sys = load('four_species.json', N=N,
+               bin_k_factor=[[mod1(x/3+0.1)] for x in range(N)], # decay A->B is modulated with x
                bin_k_factor_span=[1]
     )
 
     y0 = np.array([1.3, 1e-4, 0.7, 1e-4])
     y0 = np.concatenate([y0/(i+1)*(0.25*i**2+1) for i in range(N)])
 
-    t0 = 0.0
-    tout, yout, info = run(sys, y0, t0, tend, nt)
-    if plot:
-        for i,l in enumerate('ABCD'):
-            plt.plot(tout, yout[:,i], label=l)
-        plt.show()
+    t0 = 1e-10
+    tout = np.linspace(t0, tend, nt)
+    y = np.log(y0) if logy else y0
+    t = np.log(tout) if logt else tout
+    yout, info = run(sys, y, t)
+    if logy: yout = np.exp(yout)
 
+    cx = sys.x[:-1]+np.diff(sys.x)/2 # center x
+    if plot:
+        fig = plt.figure()
+
+        for i,l in enumerate('ABCD'):
+            ax = fig.add_subplot(3, 2, i+1, projection='3d')
+            T, X = np.meshgrid(cx, tout)
+            ax.plot_surface(T, X, yout[:,i::4], rstride=1, cstride=1, cmap=cm.YlGnBu_r)
+            ax.set_xlabel('x / m')
+            ax.set_ylabel('time / s')
+            ax.set_zlabel(r'C / mol*m**-3')
+            ax.set_title(l)
+            ax.legend(loc='best')
+
+        ax = fig.add_subplot(3, 2, 5)
+        print(sys.bin_k_factor[:,0].shape)
+        ax.plot(cx, sys.bin_k_factor[:,0])
+
+        if show:
+            plt.show()
+        else:
+            plt.savefig(__file__[:-2]+'png')
 
 if __name__ == '__main__':
     argh.dispatch_command(main)
