1e9ca68f65390b4565852ba5eb595709229f23ea
Björn Dahlgren <bjodah@gmail.com>
2014-03-09 22:57:30 +0100

    Updates radiolysis example.

--- demo/radiolysis.py
+++ demo/radiolysis.py
@@ -3,7 +3,7 @@
 
 #stdlib imports
 import json
-from math import log, e
+from math import log, e, exp
 
 #external imports
 import argh
@@ -19,18 +19,24 @@ from chemreac.integrate import run
 Demo of a large chemical reaction diffusion system.
 """
 
+YIELD_CONV = 1.0364e-07 # mol * eV / (J * molecules)
+
 name = 'aqueous_radiolysis'
 
-def main(tend=10.0, N=2, nt=1024,
+def main(t0=1e-7, tend=.1, doserate=15, N=10, nt=1024,
          plot=False, logy=False, logt=False, show=False):
 
-    sys = load(name+'.json', ReactionDiffusion, N=N, logy=logy, logt=logt)
+    null_conc = 1e-24
+
+    mu = 1.0 # linear attenuation
+    rho = 1.0 # kg/dm3
+    sys = load(name+'.json', ReactionDiffusion, N=N, logy=logy, logt=logt,
+               bin_k_factor=[[doserate*rho*YIELD_CONV*exp(-mu*i/N)] for i in range(N)])
     y0_by_name = json.load(open(name+'.y0.json', 'rt'))
-    names = json.load(open(name+'.names.json', 'rt'))
 
     # y0 with a H2 gradient
-    y0 = np.array([[y0_by_name.get(k, 1e-9) if k != 'H2' else \
-                    1e-3/(i+2) for k in names] for i in range(sys.N)])
+    y0 = np.array([[y0_by_name.get(k, null_conc) if k != 'H2' else \
+                    1e-3/(i+2) for k in sys.names] for i in range(sys.N)])
 
     t0 = 1e-7
 
@@ -47,15 +53,15 @@ def main(tend=10.0, N=2, nt=1024,
     if plot:
         import matplotlib.pyplot as plt
         for subs in ('H2', 'H2O2'):
-            plt.loglog(tout, yout[:,names.index(subs)],
+            plt.loglog(tout, yout[:,sys.names.index(subs)],
                        label=subs+'(1)')
-            plt.loglog(tout, yout[:,sys.n*(sys.N-1)+names.index(subs)],
+            plt.loglog(tout, yout[:,sys.n*(sys.N-1)+sys.names.index(subs)],
                        label=subs+'({0})'.format(sys.N))
         xlabel = "t / s"
         ylabel = "C / M"
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
-        plt.title(("{} s post radiolysis,"+\
+        plt.title(("{} s radiolysis,"+\
                    " H2 gradient ({} bins)").format(tend, sys.N))
         plt.legend(loc='best')
         if show:
