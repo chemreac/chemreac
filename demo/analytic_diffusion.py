Refactoring to use correct (and efficient) Jacobian expressions for geometries

demo/analytic_diffusion.py
@@ -7,19 +7,33 @@ import argh
 import numpy as np
 import matplotlib.pyplot as plt
 
-from chemreac import ReactionDiffusion
+from chemreac import ReactionDiffusion, FLAT, CYLINDRICAL, SPHERICAL, Geom_names
 from chemreac.integrate import run
 
-def analytic(x, t, D, mu):
-    return (4*np.pi*D*t)**-0.5*np.exp(-(x-mu)**2/(4*D*t))
+def flat_analytic(x, t, D, mu):
+    return (4*np.pi*D*t)**-0.5 * np.exp(-(x-mu)**2/(4*D*t))
 
+def spherical_analytic(x, t, D, mu):
+    return (4*np.pi*D)**-0.5 * t**-1.5 * np.exp(-(x-mu)**2/(4*D*t))
 
-def main(D=2e-3, t0=1., tend=2., x0=1., xend=2., mu=None, N=1e5, nt=30):
+def cylindrical_analytic(x, t, D, mu):
+    return (4*np.pi*D*t)**-1 * np.exp(-(x-mu)**2/(4*D*t))
+
+def main(D=2e-3, t0=1., tend=2., x0=1., xend=2., mu=None, N=1e5, nt=30, geom='f'):
     mu = mu or .5*(x0+xend)
     tout = np.linspace(t0, tend, nt)
 
+    assert geom in 'fcs'
+    geom = {'f': FLAT, 'c': CYLINDRICAL, 's': SPHERICAL}[geom]
+    print(Geom_names[geom])
+    analytic = {
+        FLAT: flat_analytic,
+        CYLINDRICAL: cylindrical_analytic,
+        SPHERICAL: spherical_analytic
+    }[geom]
+
     sys = ReactionDiffusion(
-        1, [], [], [], N, D=[D], x=np.linspace(x0, xend, N+1))
+        1, [], [], [], N, D=[D], x=np.linspace(x0, xend, N+1), geom=geom)
     y0 = analytic(sys.x_centers, t0, D, mu)
     t = tout.copy().reshape((nt,1))
     yref = analytic(sys.x_centers, t, D, mu)
@@ -35,13 +49,17 @@ def main(D=2e-3, t0=1., tend=2., x0=1., xend=2., mu=None, N=1e5, nt=30):
     for i in range(nt):
         c = 1-tout[i]/tend
         c = (1.0-c, .5-c/2, .5-c/2)
-        plt.subplot(3,1,1)
+        plt.subplot(4,1,1)
         plot(yout[i,:], c, 'Simulation (N={})'.format(sys.N))
-        plt.subplot(3,1,2)
+        plt.subplot(4,1,2)
         plot(yref[i,:], c, 'Analytic')
-        plt.subplot(3,1,3)
+        plt.subplot(4,1,3)
         plot((yref[i,:]-yout[i,:])/info['atol'], c,
              'Abs. err. / Abs. tol. (={})'.format(info['atol']))
+    plt.subplot(4,1,4)
+    plt.plot(tout, np.sum((yref-yout)**2/N, axis=1)**0.5/info['atol'])
+    plt.xlabel('Time / s')
+    plt.ylabel(r'$\sqrt{\langle E^2 \rangle} / atol$')
     plt.tight_layout()
     plt.show()
 