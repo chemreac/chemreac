Nicer concentration profile in geometric_diffusion, jacobian still wrong
Passing --with-jacobian causes numerical instability => Bug in jacobian routine.

demo/geometric_diffusion.py
@@ -26,7 +26,8 @@ Demo of chemical reaction diffusion system.
 def main(tend=10.0, N=25, nt=30, nstencil=3, lrefl=False, rrefl=False,
          with_jacobian=False):
     x = np.linspace(0.1, 1.0, N+1)
-    y0 = (x[0]/2+x[1:])**2
+    f = lambda x: 2*x**2/(x**4+1)  # f(0)=0, f(1)=1, f'(0)=0, f'(1)=0
+    y0 = f(x[1:])+x[0] #(x[0]/2+x[1:])**2
 
     geoms = (FLAT, SPHERICAL, CYLINDRICAL)
     geom_name = {FLAT: 'Flat', SPHERICAL: 'Spherical', CYLINDRICAL: 'Cylindrical'}