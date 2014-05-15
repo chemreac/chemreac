Fixed jacobian related problem (jout must be zeroed between calls).

chemreac/core.py
@@ -29,7 +29,7 @@ class ReactionDiffusionBase(object):
             k=self.k[ri])
 
     def alloc_fout(self):
-        return np.empty(self.n*self.N)
+        return np.zeros(self.n*self.N)
 
     def alloc_jout(self, banded=True, order='C', pad=0):
         if order == 'C':

chemreac/tests/test_binary_system.py
@@ -12,6 +12,8 @@ from chemreac.integrate import run
 from chemreac.serialization import load
 from chemreac.chemistry import mk_sn_dict_from_names, Reaction, ReactionSystem
 
+from test_reactiondiffusion import _test_f_and_dense_jac_rmaj
+
 """
 Test chemical reaction system with 4 species.
 (no diffusion)
@@ -169,9 +171,8 @@ def test_integrate(combo):
     assert np.allclose(tout-t0, ref_t)
 
     y = np.log(y0) if logy else y0
-    if logt:
-        tout = np.log(tout)
-    yout, info = run(rd, y, tout)
-    if logy: yout = np.exp(yout)
-
-    assert np.allclose(yout[:,:rd.n], ref_y, atol=1e-5)
+    t = np.log(tout) if logt else tout
+    _test_f_and_dense_jac_rmaj(rd, t0, y)
+    yout, info = run(rd, y, t, with_jacobian=True)
+    yout = np.exp(yout) if logy else yout
+    assert np.allclose(yout[:, :rd.n], ref_y, atol=1e-4)

chemreac/tests/test_integrate.py
@@ -3,7 +3,6 @@
 
 from __future__ import print_function, division, absolute_import, unicode_literals
 
-
 import os
 from itertools import product
 
@@ -20,6 +19,7 @@ chemical reaction system. (no diffusion)
 """
 
 LOG_COMOBS = list(product([True, False], [True, False]))
+
 
 @pytest.mark.parametrize("log", LOG_COMOBS)
 def test_decay(log):
@@ -79,7 +79,7 @@ def test_ReactionDiffusion__bin_k_factor(log_geom):
         n,
         [[i] for i in range(0,n,2)],
         [[i] for i in range(1,n,2)],
-        k=k, N=N, D=D, bin_k_factor=bkf,
+        k=k, N=N, D=D, x=x, bin_k_factor=bkf,
         bin_k_factor_span=bkf_span,
         logy=logy, logt=logt, geom=geom
     )
@@ -112,19 +112,29 @@ def test_ReactionDiffusion__bin_k_factor(log_geom):
 
 
 @pytest.mark.parametrize("N_wjac_geom", product(
-    range(5, 14, 4), (True, False), (FLAT, CYLINDRICAL, SPHERICAL)))
+    [64, 128], [False, True], (FLAT, CYLINDRICAL, SPHERICAL)))
 def test_integrate__only_1_species_diffusion__mass_conservation(N_wjac_geom):
     N, wjac, geom = N_wjac_geom
     # Test that mass convervation is fulfilled wrt diffusion.
-    x = np.linspace(0.01, 1.0, N+1)
-    y0 = (x[0]/2+x[1:])**2
+    x = np.linspace(0.01*N, N, N+1)
+    y0 = (x[0]/2/N+x[1:]/N)**2
 
-    sys = ReactionDiffusion(1, [], [], [], N=N, D=[0.02], x=x, geom=geom, nstencil=3,
+    sys = ReactionDiffusion(1, [], [], [], N=N, D=[0.02*N], x=x, geom=geom, nstencil=3,
                             lrefl=True, rrefl=True)
+    debug = False
+    if debug: # From debugging / test design
+        import matplotlib.pyplot as plt
+        plt.subplot(2,1,1)
+        plt.plot(sys.xcenters, y0)
+        plt.xlabel('x')
+        plt.ylabel('y0')
+
     tout = np.linspace(0, 10.0, 50)
-    yout, info = run(sys, y0, tout, atol=1e-12, rtol=1e-13, with_jacobian=wjac)
+    atol, rtol = 1e-6, 1e-8
+    yout, info = run(sys, y0, tout, atol=atol, rtol=rtol, with_jacobian=wjac, method='adams')
+    x /= N
     if geom == FLAT:
-        yprim = yout
+        yprim = yout*(x[1:]**1 - x[:-1]**1)
     elif geom == CYLINDRICAL:
         yprim = yout*(x[1:]**2 - x[:-1]**2)
     elif geom == SPHERICAL:
@@ -134,7 +144,13 @@ def test_integrate__only_1_species_diffusion__mass_conservation(N_wjac_geom):
 
     ybis = np.sum(yprim, axis=1)
 
-    # Need to plot this
-    print(y0)
-    print(np.average(ybis) - ybis)
-    assert np.allclose(np.average(ybis), ybis)
+    if debug: # From debugging / test design
+        plt.subplot(2,1,2)
+        plt.plot(ybis-ybis[0])
+        plt.xlabel('t')
+        plt.ylabel('tot y - tot y0')
+        plt.show()
+        print(y0)
+        print(np.average(ybis) - ybis)
+        print(np.average(ybis))
+    assert np.allclose(np.average(ybis), ybis, atol=atol, rtol=rtol)

demo/analytic_diffusion.py
@@ -94,7 +94,7 @@ def interleave(arrays, axis=0):
 
 def main(D=2e-3, t0=3., tend=7., x0=1.0, xend=2.0, mu=None, N=2048, nt=30, geom='f',
          logt=False, logy=False, random=False, k=0.0, nstencil=3, lrefl=False, rrefl=False,
-         with_jacobian=False):
+         num_jacobian=False):
     decay = (k != 0.0)
     mu = float(mu or x0)
     tout = np.linspace(t0, tend, nt)
@@ -138,7 +138,7 @@ def main(D=2e-3, t0=3., tend=7., x0=1.0, xend=2.0, mu=None, N=2048, nt=30, geom=
     y = np.log(y0) if logy else y0
     t = np.log(tout) if logt else tout
     yout, info = run(sys, y, t, atol=(xend-x0)**2/1e9, rtol=1e-8,
-                     with_jacobian=with_jacobian, method='bdf')
+                     with_jacobian=(not num_jacobian), method='bdf')
     if logy: yout = np.exp(yout)
     print(info)
 

demo/geometric_diffusion.py
@@ -24,7 +24,7 @@ Demo of chemical reaction diffusion system.
 # <geometric_diffusion.png>
 
 def main(tend=10.0, N=25, nt=30, nstencil=3, lrefl=False, rrefl=False,
-         with_jacobian=False):
+         num_jacobian=False):
     x = np.linspace(0.1, 1.0, N+1)
     f = lambda x: 2*x**2/(x**4+1)  # f(0)=0, f(1)=1, f'(0)=0, f'(1)=0
     y0 = f(x[1:])+x[0] #(x[0]/2+x[1:])**2
@@ -41,7 +41,7 @@ def main(tend=10.0, N=25, nt=30, nstencil=3, lrefl=False, rrefl=False,
     for G in geoms:
         sys = ReactionDiffusion(1, [], [], [], N=N, D=[0.02], x=x,
                                 geom=G, nstencil=nstencil, lrefl=lrefl, rrefl=rrefl)
-        yout, info = run(sys, y0, tout, with_jacobian=with_jacobian)
+        yout, info = run(sys, y0, tout, with_jacobian=(not num_jacobian))
         res.append(yout)
 
     for i, G in enumerate(geoms):

demo/radiolysis.py
@@ -41,7 +41,7 @@ def main(t0=1e-7, tend=.1, doserate=15, N=10, nt=1024,
     tout = np.logspace(log(t0), log(tend), nt+1, base=e)
     y = np.log(y0.flatten()) if logy else y0.flatten()
     t = np.log(tout) if logt else tout
-    yout, info = run(sys, y, t, with_jacobian=False)
+    yout, info = run(sys, y, t)
     if logy: yout = np.exp(yout)
     #if logt: tout = np.exp(tout)
 