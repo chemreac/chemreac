ac237155114106e603a9d6f15b1f34bc0b48a7ac
Björn Dahlgren <bjodah@gmail.com>
2014-06-09 16:47:15 +0200

    Update test script for drone.io

--- scripts/test_drone.io.sh
+++ scripts/test_drone.io.sh
@@ -25,6 +25,6 @@ conda info -a
 conda create -n test-env python=${PY_VERSION} numpy=1.8.1 scipy=0.14 cython=0.20.1
 source activate test-env
 
-pip install --upgrade --user argh mako quantities pytest https://github.com/bjodah/pycompilation/archive/v0.2.21.tar.gz https://github.com/sympy/sympy/archive/master.zip
+pip install argh mako quantities pytest https://github.com/bjodah/pycompilation/archive/v0.2.21.tar.gz https://github.com/sympy/sympy/archive/master.zip
 python setup.py install
 py.test
