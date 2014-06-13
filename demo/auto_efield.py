Corrected div(v*c) formula, work on auto_efield demo.

demo/auto_efield.py
@@ -14,22 +14,39 @@ from chemreac import (
 from chemreac.integrate import run
 
 
-def gaussian(x, mu, sigma, logy, logx):
-    x = np.exp(x) if logx else x
-    a = 1/sigma/(2*np.pi)**0.5
+def gaussian(x, mu, sigma, logy, logx, geom):
+    if geom == FLAT:
+        a = 1/sigma/(2*np.pi)**0.5
+    elif geom == CYLINDRICAL:
+        a = 1/sigma/(2*np.pi)**0.5
+    elif geom == SPHERICAL:
+        a = 1/sigma/(2*np.pi)**0.5
+    else:
+        raise RuntimeError()
     b = -0.5*((x-mu)/sigma)**2
     if logy:
         return log(a) + b
     else:
         return a*np.exp(b)
 
+def pair_of_gaussians(x, offsets, sigma, logy, logx, geom):
+    x = np.exp(x) if logx else x
+    xspan = (x[-1] - x[0])
+    xl = x[0] + (0.5 + offsets[0])*xspan  # lower
+    xu = x[0] + (0.5 + offsets[1])*xspan  # upper
+    return (
+        gaussian(x, xl, sigma, logy, logx, geom),
+        gaussian(x, xu, sigma, logy, logx, geom)
+    )
 
-def integrate_rd(D=2e-4, t0=3., tend=7., x0=0.1, xend=1.0, N=128, offset=0.01,
-                 mobility=1e-6, nt=25, geom='f', logt=False, logy=False,
-                 logx=False, random=False, nstencil=3, lrefl=False,
-                 rrefl=False, num_jacobian=False, method='bdf', plot=False,
+
+def integrate_rd(D=0., t0=1e-6, tend=7., x0=0.1, xend=1.0, N=256,
+                 offset=0.25, mobility=3e-7, nt=25, geom='f',
+                 logt=False, logy=False, logx=False, random=False,
+                 nstencil=3, lrefl=False, rrefl=False,
+                 num_jacobian=False, method='bdf', plot=False,
                  atol=1e-6, rtol=1e-6, random_seed=42, surf_chg=0.0,
-                 sigma_q=23):
+                 sigma_q=101):
     if random_seed:
         np.random.seed(random_seed)
     n = 2
@@ -71,19 +88,18 @@ def integrate_rd(D=2e-4, t0=3., tend=7., x0=0.1, xend=1.0, N=128, offset=0.01,
     )
 
     # Initial conditions
-    sigma = (xend-x0)/sigma_q
-    y0 = 0.13*np.vstack((
-        gaussian(sys.xcenters, x0+(0.5+offset)*(xend-x0), sigma, logy, logx),
-        gaussian(sys.xcenters, x0+(0.5-offset)*(xend-x0), sigma, logy, logx)
-    )).transpose()
+    s = (xend-x0)/sigma_q
+    y0 = np.vstack(pair_of_gaussians(sys.xcenters, [offset, -offset],
+                                     s, logy, logx, geom)).transpose()
 
     if plot:
         # Plot initial E-field
         import matplotlib.pyplot as plt
         sys.calc_efield(y0.flatten())
-        plt.subplot(3, 1, 3)
+        plt.subplot(4, 1, 3)
         plt.plot(sys.xcenters, sys.efield)
         plt.plot(sys.xcenters, sys.xcenters*0)
+
     # Run the integration
     t = np.log(tout) if logt else tout
     yout, info = run(sys, y0.flatten(), t,
@@ -93,41 +109,52 @@ def integrate_rd(D=2e-4, t0=3., tend=7., x0=0.1, xend=1.0, N=128, offset=0.01,
 
     # Plot results
     if plot:
-        def _plot(y, ttl=None, apply_exp_on_y=False, **kwargs):
-            plt.plot(sys.xcenters, np.exp(y) if apply_exp_on_y else y,
+        def _plot(y, ttl=None, exp_on_y=False, **kwargs):
+            plt.plot(sys.xcenters, np.exp(y) if exp_on_y else y,
                      **kwargs)
             if N < 100:
-                plt.vlines(sys.x, 0, np.ones_like(sys.x)*max(y), linewidth=.1,
-                           colors='gray')
+                plt.vlines(sys.x, 0, np.ones_like(sys.x)*max(y),
+                           linewidth=.1, colors='gray')
             plt.xlabel('x / m')
             plt.ylabel('C / M')
             if ttl:
                 plt.title(ttl)
 
         for i in range(nt):
-            plt.subplot(3, 1, 1)
+            plt.subplot(4, 1, 1)
             c = 1-tout[i]/tend
             c = (1.0-c, .5-c/2, .5-c/2)
             _plot(yout[i, :, 0], 'Simulation (N={})'.format(sys.N),
-                  apply_exp_on_y=logy, c=c, label='$z_A=1$' if i==0 else None)
-            _plot(yout[i, :, 1], apply_exp_on_y=logy, c=c[::-1],
+                  exp_on_y=logy, c=c, label='$z_A=1$' if i==0 else None)
+            _plot(yout[i, :, 1], exp_on_y=logy, c=c[::-1],
                   label='$z_B=-1$' if i==0 else None)
             plt.legend()
 
-            plt.subplot(3, 1, 2)
+            plt.subplot(4, 1, 2)
             if logy:
                 delta_y = np.exp(yout[i, :, 0]) - np.exp(yout[i, :, 1])
             else:
                 delta_y = yout[i, :, 0] - yout[i, :, 1]
             _plot(delta_y, 'Diff'.format(sys.N),
-                  apply_exp_on_y=logy,
+                  exp_on_y=logy,
                   c=[c[2], c[0], c[1]],
                   label='A-B (positive excess)' if i==0 else None)
             plt.legend()
             plt.xlabel('Time / s')
             plt.ylabel(r'C')
-        plt.subplot(3, 1, 3)
+        plt.subplot(4, 1, 3)
         plt.plot(sys.xcenters, sys.efield)
+
+        for i in range(3):
+            plt.subplot(4, 1, i+1)
+            ax = plt.gca()
+            for d in (-1, 1):
+                plt.plot([x0+(0.5+d*offset)*(xend-x0)]*2,
+                         ax.get_ylim(), '--k')
+        plt.subplot(4, 1, 4)
+        for i in range(n):
+            plt.plot(tout, [sys.integrated_conc(yout[j,:,i]) for j in range(nt)])
+        plt.tight_layout()
         plt.show()
     return tout, yout, info, sys
 

demo/tests/test_auto_efield.py
@@ -18,12 +18,25 @@ TR_FLS = (True, False)
     TR_FLS, TR_FLS, TR_FLS, TR_FLS, TR_FLS, TR_FLS, [3, 5], 'fcs')))
 def test_perfect_overlap(params):
     ly, lt, lx, r, lr, rr, ns, geom = params
-    tout, yout, info, sys = integrate_rd(
-        geom=geom, logt=lt, logy=ly, logx=lx,
-        N=64, random=r, nstencil=ns,
-        lrefl=lr, rrefl=rr, atol=1e-6, rtol=1e-6, offset=0)
+    kwargs = {'geom': geom, 'logt': lt, 'logy': ly, 'logx': lx,
+              'random': r, 'nstencil': ns, 'lrefl': lr, 'rrefl': rr}
+
+    # zero offset
+    tout, yout, info, sys = integrate_rd(D=1e-3, N=64,
+                                         offset=0, **kwargs)
     if ly:
         delta_y = np.exp(yout[:, :, 0]) - np.exp(yout[:, :, 1])
     else:
         delta_y = yout[:, :, 0] - yout[:, :, 1]
     assert np.all(np.abs(delta_y) < 1e-3)
+
+    # separated gaussians
+    tout, yout, info, sys = integrate_rd(
+        D=0.0, t0=1e-6, tend=7, x0=0.1, xend=1.0, N=64,
+        offset=.25, mobility=3e-7, nt=25, sigma_q=101, **kwargs)
+    mass_consv = np.sum(np.exp(yout) if ly else yout, axis=1)
+    #assert np.all(mass_consv - mass_consv[[0],:] < 1e-6)
+    if geom == 'f':
+        efield_i = sys.calc_efield(yout[0,:,:].flatten())
+        efield_f = sys.calc_efield(yout[-1,:,:].flatten())
+        assert np.max(np.abs(efield_i)) - np.max(np.abs(efield_f)) < 1e-6