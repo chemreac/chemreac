8055d4a7934d610f525c0a67de5920de8cfce4b3
Björn Dahlgren <bjodah@gmail.com>
2015-08-05 12:13:03 +0200

    New docker image for distribution.

---
+++ scripts/docker_conda_distribution/README.rst
@@ -0,0 +1,9 @@
+Ubuntu 14.04 with miniconda3 and chemreac
+=========================================
+
+Comes with miniconda3 installed in $MINICONDA_PATH, 
+$PATH also has $MINICONDA_PATH/bin in it.
+
+This image essentially provides a working chemreac installation.
+
+See Dockerfile for installed packages.
--- scripts/install_sundials_w_lapack.sh
+++ scripts/install_sundials_w_lapack.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+BUILD_TYPE=${1:-Debug}
 SUNDIALS_URLS=(\
 "http://hera.physchem.kth.se/~bjorn/sundials-2.5.0.tar.gz" \
-"http://computation.llnl.gov/casc/sundials/download/code/sundials-2.5.0.tar.gz" \
 "http://ftp.mcs.anl.gov/pub/petsc/externalpackages/sundials-2.5.0.tar.gz" \
 "http://pkgs.fedoraproject.org/repo/pkgs/sundials/sundials-2.5.0.tar.gz/aba8b56eec600de3109cfb967aa3ba0f/sundials-2.5.0.tar.gz" \
+"http://computation.llnl.gov/casc/sundials/download/code/sundials-2.5.0.tar.gz" \
 )
 
 SUNDIALS_FNAME="sundials-2.5.0.tar.gz"
@@ -21,7 +22,7 @@ for URL in "${SUNDIALS_URLS[@]}"; do
             tar xzf $SUNDIALS_FNAME
             mkdir sundials_build
             cd sundials_build
-            cmake -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_BUILD_TYPE:STRING="Debug" -DLAPACK_ENABLE:BOOL=1 ../sundials-*/
+            cmake -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_BUILD_TYPE:STRING="$1" -DLAPACK_ENABLE:BOOL=1 ../sundials-*/
             if [[ $? != 0 ]]; then
                 echo "cmake of sundials failed."
                 exit 1
