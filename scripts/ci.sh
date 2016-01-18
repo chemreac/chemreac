d8086ec2804974fddbf9f7be0b47259135ea081f
Björn Dahlgren <bjodah@gmail.com>
2016-01-18 10:33:56 +0100

    Remove build files

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,8 +1,10 @@
-#!/bin/bash -xeu
+#!/bin/bash -xu
 if [[ "$CI_BRANCH" =~ ^v[0-9]+.[0-9]?* ]]; then
     eval export ${1^^}_RELEASE_VERSION=\$CI_BRANCH
     echo ${CI_BRANCH} | tail -c +2 > __conda_version__.txt
 fi
+rm -r build/ dist/* */*.so
+set +e
 python2 setup.py sdist
 pip install dist/*.tar.gz
 (cd /; python2.7 -m pytest --pyargs $1)
