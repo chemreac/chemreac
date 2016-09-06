056311e7d71d2bd5b58401fa478954acc770c44b
Björn Dahlgren <bjodah@gmail.com>
2016-09-06 15:49:39 +0200

    skip test_aqueous_radiolysis.py::test_integrate_rd under Python 3

--- examples/tests/test_aqueous_radiolysis.py
+++ examples/tests/test_aqueous_radiolysis.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import sys
+
+import pytest
+
 from aqueous_radiolysis import integrate_rd
 from chemreac.util.testing import veryslow
 
 
 @veryslow
+@pytest.mark.skipif(sys.version_info[0] > 2, reason='pytest/python3 incompatible with this test')
 def test_integrate_rd():
     integr = integrate_rd()
     assert integr.info['success']
