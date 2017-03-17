ba76a3e22a91735c534b0d70c8b316565a1a7da0
Björn Dahlgren <bjodah@gmail.com>
2017-03-17 12:57:43 +0100

    Adapt to upstream ChemPy

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,7 +2,7 @@
 # Usage
 #   $ ./scripts/run_tests.sh
 # or
-#   $ ./scripts/run_tests.sh --cov pycvodes --cov-report html --slow --veryslow
+#   $ ./scripts/run_tests.sh --cov pycvodes --cov-report html
 ${PYTHON:-python} setup.py build_ext -i
-${PYTHON:-python} -m pytest --doctest-modules --pep8 --flakes $@
+${PYTHON:-python} -m pytest --doctest-modules --pep8 --flakes --slow --veryslow $@
 ${PYTHON:-python} -m doctest README.rst
