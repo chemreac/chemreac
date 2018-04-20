e2d30fa6785ccb932072cf86fefc560873d93ebc
Björn Dahlgren <bjodah@gmail.com>
2018-04-20 14:57:19 +0200

    Added CircleCI config

--- scripts/build-conda-recipe-using-docker.sh
+++ scripts/build-conda-recipe-using-docker.sh
@@ -10,7 +10,12 @@ REPOPATH=$(unset CDPATH && cd "$(dirname "$0")/.." && echo $PWD)
 if [ ! -d "$REPOPATH/dist" ]; then
     mkdir "$REPOPATH/dist"
 fi
-CMD="set -xe; conda-build "${@:2}" --output-folder /mount/dist /mount/$1"
+CMD="set -xe; "
+for CONDA_CHANNEL in ${CONDA_CHANNELS:-bjodah}; do
+    ADDCHANNEL="conda config --add channels ${CONDA_CHANNEL}; "
+    CMD="$CMD $ADDCHANNEL"
+done
+CMD="$CMD conda-build "${@:2}" --output-folder /mount/dist /mount/$1"
 set -x
 CONTAINERNAME=anfilte-$(basename $REPOPATH)
 HOST_USER=${SUDO_USER:-${LOGNAME}}
