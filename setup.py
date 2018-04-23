b55c8d305bafc0da00ca609c7f5a13715ef6d8a8
Björn Dahlgren <bjodah@gmail.com>
2018-04-23 20:19:28 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ _WITH_OPENMP = env['WITH_OPENMP'] == '1'
 _WITH_DATA_DUMPING = env['WITH_DATA_DUMPING'] == '1'
 
 # Source distributions contain rendered sources
-_common_requires = ['numpy>=1.11', 'block_diag_ilu>=0.3.3', 'pycvodes>=0.11.4', 'finitediff>=0.4.0']
+_common_requires = ['numpy>=1.11', 'block_diag_ilu>=0.3.3', 'pycvodes>=0.10.4', 'finitediff>=0.4.0']
 setup_requires = _common_requires + ['mako>=1.0']
 install_requires = _common_requires + ['chempy>=0.6.7', 'quantities>=0.12.1']
 package_include = os.path.join(pkg_name, 'include')
