aea5efbbf5da66e03b2792317741487489a08852
Björn Dahlgren <bjodah@gmail.com>
2018-05-01 11:43:46 +0200

    script fix

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -8,14 +8,13 @@ fi
 set -e
 python3 -m pip install --user -e .[all]
 ./scripts/run_tests.sh ${@:2}
-python3 -m pip uninstall $PKG_NAME
+python3 -m pip uninstall -y $PKG_NAME
 
 python3 setup.py sdist
 cp dist/${PKG_NAME}-*.tar.gz /tmp
 (cd /; python3 -m pip install --force-reinstall /tmp/${PKG_NAME}-*.tar.gz; python3 -c "import $PKG_NAME")
-python3 -m pip uninstall $PKG_NAME
+python3 -m pip uninstall -y $PKG_NAME
 
 # Make sure repo is pip installable from git-archive zip
 git archive -o /tmp/$PKG_NAME.zip HEAD
 (cd /; python3 -m pip install --force-reinstall /tmp/$PKG_NAME.zip; python3 -c "import ${PKG_NAME}")
-python3 -m pip uninstall $PKG_NAME
