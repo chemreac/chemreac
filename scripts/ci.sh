bd0b2563dd18b3e6ae93d95e48ab1a4035db10e0
Björn Dahlgren <bjodah@gmail.com>
2018-06-14 09:23:53 +0200

    skip lsan for now

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -20,7 +20,7 @@ CC=clang-6.0 \
   CXX=clang++-6.0 \
   CFLAGS="-fsanitize=address -UNDEBUG" \
   python3 setup.py build_ext -i
-LD_PRELOAD=/usr/lib/llvm-6.0/lib/clang/6.0.0/lib/linux/libclang_rt.asan-x86_64.so ./scripts/run_tests.sh ${@:2}
+ASAN_OPTIONS=detect_leaks=0 LD_PRELOAD=/usr/lib/llvm-6.0/lib/clang/6.0.0/lib/linux/libclang_rt.asan-x86_64.so ./scripts/run_tests.sh ${@:2}
 python3 -m pip uninstall -y $PKG_NAME
 
 python3 setup.py sdist
