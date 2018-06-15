7f7b2cb9d683c59227ee80a13ce448ed0f752e2e
Björn Dahlgren <bjodah@gmail.com>
2018-06-15 16:52:00 +0200

    update ci.sh (#85)
    * update ci.sh

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -5,16 +5,17 @@ if [[ "$CI_BRANCH" =~ ^v[0-9]+.[0-9]?* ]]; then
     echo ${CI_BRANCH} | tail -c +2 > __conda_version__.txt
 fi
 
-python3 -m pip install --user argh finitediff block_diag_ilu pycvodes
+python3 -m pip install argh finitediff block_diag_ilu pycvodes
 
 set -e
 
 (cd tests-native; make -B CONTEXT=valgrind EXTRA_COMPILE_ARGS='-D_GLIBCXX_DEBUG' test)
 (cd tests-native; make -B CXX=clang++-6.0 CC=clang-6.0 OPTIMIZE=1 WITH_OPENMP=0 EXTRA_COMPILE_ARGS='-fsanitize=address -DNDEBUG' test)
 
-python3 -m pip install --user -e .[all]
-#ulimit -v 2048000
-./scripts/run_tests.sh
+python3 -m pip install -e .[all]
+git clean -xfd
+CFLAGS="-D_GLIBCXX_DEBUG" python3 setup.py develop
+bash -c "ulimit -v 2048000; ./scripts/run_tests.sh"
 git clean -xfd
 CC=clang-6.0 \
   CXX=clang++-6.0 \
@@ -26,7 +27,6 @@ git clean -xfd
 python3 setup.py sdist
 cp dist/${PKG_NAME}-*.tar.gz /tmp
 (cd /; python3 -m pip install --force-reinstall /tmp/${PKG_NAME}-*.tar.gz; python3 -c "import $PKG_NAME")
-python3 -m pip uninstall -y $PKG_NAME
 
 # Make sure repo is pip installable from git-archive zip
 git archive -o /tmp/$PKG_NAME.zip HEAD
