96821453cdae535cb0b7f1b927ed885a31f5f7da
Björn Dahlgren <bjodah@gmail.com>
2018-08-12 15:57:25 +0200

    Update LD_PRELOAD_PATH

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -20,7 +20,7 @@ CC=clang-6.0 \
   CXX=clang++-6.0 \
   CFLAGS="-fsanitize=address -UNDEBUG" \
   python3 setup.py build_ext -i
-ASAN_OPTIONS=detect_leaks=0 LD_PRELOAD=/usr/lib/llvm-6.0/lib/clang/6.0.0/lib/linux/libclang_rt.asan-x86_64.so ./scripts/run_tests.sh ${@:2}
+ASAN_OPTIONS=detect_leaks=0 LD_PRELOAD=/usr/lib/llvm-6.0/lib/clang/6.0.1/lib/linux/libclang_rt.asan-x86_64.so ./scripts/run_tests.sh ${@:2}
 git clean -xfd
 
 python3 setup.py sdist
