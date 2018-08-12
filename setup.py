2f44df4dc437595dee846c226af98efe29ced770
Björn Dahlgren <bjodah@gmail.com>
2018-08-12 17:01:55 +0200

    Fix setup.py, update manifest

--- setup.py
+++ setup.py
@@ -63,10 +63,6 @@ _WITH_DEBUG = env['WITH_DEBUG'] == '1'
 _WITH_OPENMP = env['WITH_OPENMP'] == '1'
 _WITH_DATA_DUMPING = env['WITH_DATA_DUMPING'] == '1'
 
-# Source distributions contain rendered sources
-_common_requires = ['numpy>=1.11', 'block_diag_ilu>=0.4.0', 'pycvodes>=0.11.4', 'finitediff>=0.6.2']
-setup_requires = _common_requires + ['mako>=1.0'] + (['cython'] if USE_CYTHON else []),
-install_requires = _common_requires + ['chempy>=0.6.7', 'quantities>=0.12.1']
 package_include = os.path.join(pkg_name, 'include')
 
 USE_CYTHON = None
@@ -103,7 +99,7 @@ if len(sys.argv) > 1 and '--help' not in sys.argv[1:] and sys.argv[1] not in (
         USE_CYTHON = False
     else:
         pyx_path = 'chemreac/_chemreac.pyx'
-        USE_CYTHON = os.path.exists(pyx_path)
+        USE_CYTHON = not os.path.exists(_path_under_setup(pkg_name, '_chemreac.cpp'))
 
     ext_modules.append(Extension('chemreac._chemreac', ['chemreac/_chemreac' + ('.pyx' if USE_CYTHON else '.cpp')]))
 
@@ -151,6 +147,11 @@ long_description = io.open(_path_under_setup('README.rst'),
 if not len(long_description) > 100:
     warnings.warn("Long description from README.rst probably not read correctly.")
 _author, _author_email = io.open(_path_under_setup('AUTHORS'), 'rt', encoding='utf-8').readline().split('<')
+
+# Source distributions contain rendered sources
+_common_requires = ['numpy>=1.11', 'block_diag_ilu>=0.4.0', 'pycvodes>=0.11.4', 'finitediff>=0.6.2']
+setup_requires = _common_requires + ['mako>=1.0'] + (['cython'] if USE_CYTHON else []),
+install_requires = _common_requires + ['chempy>=0.6.7', 'quantities>=0.12.1']
 
 setup_kwargs = dict(
     name=pkg_name,
