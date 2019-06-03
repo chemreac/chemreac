e6a66766d0ab352074935d64eea1f76073b7ff7a
Björn Ingvar Dahlgren <bjodah@gmail.com>
2019-06-03 16:58:13 +0200

    CI: updates

--- .ci/copy_tree.sh
+++ .ci/copy_tree.sh
@@ -1,14 +1,16 @@
-if [ "$#" -ne 3 ]; then
-    2>&1 echo "Three arguments needed (destination folder, symengine folder, sundials base)"
+if [ "$#" -ne 2 ]; then
+    2>&1 echo "Two arguments needed (destination folder, sundials base)"
     exit 1
 fi
 mkdir -p "$1"
-cp -ra external scripts kinetgine tests dist py-ext-mod util *.* .ci "$1"
+cp -ra external scripts chemreac tests-native examples *.* .ci "$1"
 cp -ra ci-cache/pyusrb /opt
+export PYTHONUSERBASE=/opt/pyusrb
+
 export SUNDBASE=$2
 export CPATH=$SUNDBASE/include:$CPATH
 export LIBRARY_PATH=$SUNDBASE/lib
 export LD_LIBRARY_PATH=$SUNDBASE/lib
-export CMAKE_PREFIX_PATH=$SUNDBASE:$3:$CMAKE_PREFIX_PATH
+export CMAKE_PREFIX_PATH=$SUNDBASE:$CMAKE_PREFIX_PATH
 cd $1
 ${PYTHON:-python3} -c "import pycvodes"
