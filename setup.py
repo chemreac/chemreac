9778f7c5e31bdde82f5e4293a7a88cae571359e4
Björn Ingvar Dahlgren <bjodah@gmail.com>
2019-06-04 14:36:03 +0200

    bump CI image

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ _WITH_OPENMP = env['WITH_OPENMP'] == '1'
 _WITH_DATA_DUMPING = env['WITH_DATA_DUMPING'] == '1'
 
 # Source distributions contain rendered sources
-_common_requires = ['numpy>=1.11', 'block_diag_ilu>=0.3.9,<0.4.0', 'pycvodes>=0.10.12,<0.11.0', 'finitediff>=0.6.2']
+_common_requires = ['numpy>=1.11', 'block_diag_ilu>=0.3.10,<0.4.0', 'pycvodes>=0.10.12,<0.11.0', 'finitediff>=0.6.2']
 setup_requires = _common_requires + ['mako>=1.0']
 install_requires = _common_requires + ['chempy>=0.6.8,<0.7.0', 'quantities>=0.12.1']
 package_include = os.path.join(pkg_name, 'include')
