3a0f8bef939fafeb46046725d3b31ef6c531424e
Björn Dahlgren <bjodah@gmail.com>
2019-07-22 15:35:12 +0200

    implement get_dx_max and get_dx0 (#107)
    * implement get_dx_max and get_dx0
    
    * handle get_dx0 when limits unset
    
    * tweaks around clip_to_pos
    
    * fix compiler warnings
    
    * tweak tests
    
    * tweak build
    
    * Tweak CI
    
    * debug CI
    
    * Debugging build_docs step
    
    * debugging /tmp/ci/dist moving
    
    * Remove build-dir
    
    * Tweak LD_LIBRARY_PATH
    
    * Better bounds error text
    
    * Use clog instead of cerr (buffered)
    
    * Add some guards
    
    * Add get_dx_max_upper_limit

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -5,28 +5,29 @@ if [[ "$CI_BRANCH" =~ ^v[0-9]+.[0-9]?* ]]; then
 fi
 
 python3 -m pip install --user -e .[all]
+python3 setup.py build_ext -i
 mkdir -p dist
 cp -r chemreac dist/.
-
+ls dist/chemreac
+rm -r build
 set -e
 
 (cd tests-native; make -B CONTEXT=valgrind EXTRA_COMPILE_ARGS='-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC' test)
 (cd tests-native; make -B CXX=clang++-8 CC=clang-8 OPTIMIZE=1 WITH_OPENMP=0 EXTRA_COMPILE_ARGS='-fsanitize=address -DNDEBUG' test)
 
-rm -r build/
 CFLAGS="-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC" python3 setup.py build_ext -i
 bash -c "ulimit -v 2048000; ./scripts/run_tests.sh"
 
-rm -r build/
+rm -rf build/
 CC=clang-8 \
   CXX=clang++-8 \
   CFLAGS="-fsanitize=address -UNDEBUG" \
   python3 setup.py build_ext -i
-ASAN_OPTIONS=detect_leaks=0 LD_PRELOAD=/usr/lib/llvm-8/lib/clang/8.0.1/lib/linux/libclang_rt.asan-x86_64.so ./scripts/run_tests.sh "${@:2}"
+PYTHONMALLOC=malloc ASAN_OPTIONS=detect_leaks=0 LD_PRELOAD=/usr/lib/llvm-8/lib/clang/8.0.1/lib/linux/libclang_rt.asan-x86_64.so ./scripts/run_tests.sh "${@:2}"
 
 python3 -m pip uninstall -y chemreac
 
-rm -r build/
+rm -rf build/
 
 python3 setup.py sdist
 cp dist/${PKG_NAME}-*.tar.gz /tmp
