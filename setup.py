690986c8df4329043811a5722866f988c3b7e3ea
Björn Ingvar Dahlgren <bjodah@gmail.com>
2020-03-09 22:54:56 +0100

    req. batemaneq>=0.2.2

--- setup.py
+++ setup.py
@@ -183,7 +183,7 @@ setup_kwargs = dict(
     install_requires=install_requires,
     extras_require={'all': [
         'argh', 'pytest', 'scipy>=0.19.1', 'matplotlib', 'mpld3',
-        'sym>=0.3.4', 'sympy>=1.1.1,!=1.2', 'pyodeint>=0.10.1', 'pygslodeiv2>=0.9.1', 'batemaneq>=0.2.1',
+        'sym>=0.3.4', 'sympy>=1.1.1,!=1.2', 'pyodeint>=0.10.1', 'pygslodeiv2>=0.9.1', 'batemaneq>=0.2.2',
         'sphinx', 'sphinx_rtd_theme', 'numpydoc', 'pyodesys>=0.12.6'
     ]},
     python_requires='>=3.6',
