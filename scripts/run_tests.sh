61c94e80c1f863dde0d554ead160afe9b0e064b7
Björn Ingvar Dahlgren <bjodah@gmail.com>
2020-11-23 23:13:37 +0100

    skip pep8

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,5 +4,5 @@
 # or
 #   $ ./scripts/run_tests.sh --cov pycvodes --cov-report html
 export MPLBACKEND=Agg
-${PYTHON:-python3} -m pytest --doctest-modules --pep8 --flakes $@
+${PYTHON:-python3} -m pytest --doctest-modules --flakes $@
 ${PYTHON:-python3} -m doctest README.rst
